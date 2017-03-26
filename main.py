b6b90c800933588d5758636879aa6702f0c41c22
bryandobberstein <bryan.dobberstein@att.net>
2017-03-26 15:03:43 -0500

    Add fuction and string substitution to remove redundant code

--- main.py
+++ main.py
@@ -2,24 +2,26 @@ import webapp2
 from cgi import escape
 from caesar import encrypt
 
-form1 = '''<form method = "post" action = "">
-<label>ROT <input type = "number" min = "-26" max = "26" name = "r" value = '''
-form2 = '''></label><br>
-<textarea rows = "4" cols = "50" name = "t" placeholder = "Enter text to be encypered...">'''
-form3 = '''</textarea><br>
-<input type = "submit">
+form = '''
+    <form method = "post" action = "">
+    <label>ROT <input type = "number" min = "-26" max = "26" name = "r" value = %d></label><br>
+    <textarea rows = "4" cols = "50" name = "t" placeholder = "Enter text to be encypered...">%s</textarea><br>
+    <input type = "submit">
     </form>
 '''
 
 class MainHandler(webapp2.RequestHandler):
+    def write_form(self, num = 0, txt = ""):
+        txt = escape(txt)
+        self.response.out.write(form %(num, txt))
+
     def get(self):
-        self.response.out.write(form1 + form2 + form3)
+        self.write_form()
 
     def post(self):
         r = int(self.request.get("r"))
         t = str(self.request.get("t"))
-        t = t
         ctext = encrypt(t, r)
-        self.response.out.write(form1 + str(r) + form2 + escape(ctext) + form3)
+        self.write_form(r, ctext)
 
 app = webapp2.WSGIApplication([('/', MainHandler)], debug = True)
