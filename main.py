b7912f9e0ae92497729d7648e436766c332acdb6
bryandobberstein <bryan.dobberstein@att.net>
2017-03-26 11:57:35 -0500

    Reduced redundant form code

--- main.py
+++ main.py
@@ -1,31 +1,26 @@
 import webapp2
+from cgi import escape
 from caesar import encrypt
 
-form = '''
-    <form method = "post" action = "cyphered">
-        <label>ROT </label><input type  = "number" min ="-26" max = "26" name = "r"><br>
-        <label>Text <textarea name = "t"></textarea></label><br>
-        <input type = "submit">
+form1 = '''<form method = "post" action = "cyphered">
+<label>ROT <input type = "number" min = "-26" max = "26" name = "r" value = '''
+form2 = '''></label><br>
+<label>Text <textarea name = "t">'''
+form3 = '''</textarea></label><br>
+<input type = "submit">
     </form>
 '''
 
 class MainHandler(webapp2.RequestHandler):
     def get(self):
-        self.response.out.write(form)
+        self.response.out.write(form1 + form2 + form3)
 
 class EncryptHandler(webapp2.RequestHandler):
     def post(self):
         r = int(self.request.get("r"))
         t = str(self.request.get("t"))
+        t = t
         ctext = encrypt(t, r)
-        newform1 = '<form method = "post" action = "cyphered">'
-        newform2 = '<label>ROT <input type = "number" min = "-26" max = "26" name = "r" value = ' 
-        newform3 = '></label><br>'
-        newform4 = '<label>Text <textarea name = "t">'
-        newform5 = '''</textarea></label><br>
-        <input type = "submit">
-            </form>
-        '''
-        self.response.out.write(newform1 + "\n" + newform2 + str(r) + newform3 + "\n" + newform4 + ctext + newform5)
+        self.response.out.write(form1 + str(r) + form2 + escape(ctext) + form3)
 
 app = webapp2.WSGIApplication([('/', MainHandler), ('/cyphered', EncryptHandler)], debug = True)
