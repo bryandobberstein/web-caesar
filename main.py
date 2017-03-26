2e7f4c3267877b3951ac7f9e13bb38e92653426b
bryandobberstein <bryan.dobberstein@att.net>
2017-03-25 23:31:04 -0500

    Put output on form that allows further encryption/decryption

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from caesar import encrypt
 form = '''
     <form method = "post" action = "cyphered">
         <label>ROT <input type = "text" name = "r"></label><br>
-        <label>Text <input type = "text" name = "t"></label><br>
+        <label>Text <input type = "text" name = "t" value = "plain text"></label><br>
         <input type = "submit">
     </form>
 '''
@@ -17,6 +17,15 @@ class EncryptHandler(webapp2.RequestHandler):
     def post(self):
         r = int(self.request.get("r"))
         t = str(self.request.get("t"))
-        self.response.out.write(encrypt(t, r))
+        ctext = encrypt(t, r)
+        newform1 = '<form method = "post" action = "cyphered">'
+        newform2 = '<label>ROT <input type = "text" name = "r" value = ' 
+        newform3 = '></label><br>'
+        newform4 = '<label>Text <input type = "text" name = "t" value = '
+        newform5 = '''></label><br>
+        <input type = "submit">
+            </form>
+        '''
+        self.response.out.write(newform1 + newform2 + str(r) + newform3 + newform4 + ctext + newform5)
 
 app = webapp2.WSGIApplication([('/', MainHandler), ('/cyphered', EncryptHandler)], debug = True)
